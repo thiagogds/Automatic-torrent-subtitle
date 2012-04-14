86e1dd69b76e0e606c9df2bbdf7f19eb9ad1861e
Thiago Garcia <thiagogds14@gmail.com>
2012-04-14 17:29:48 -0300

    Trata arquivos que tenham 720p e HDTV

--- get_sub_name.py
+++ get_sub_name.py
@@ -2,11 +2,14 @@ import sys
 
 full_name = sys.argv[1]
 short_name = ''
-
-if full_name.find('.HDTV') != -1:
-    short_name = full_name[:full_name.find('.HDTV')]
+found = False
 
 if full_name.find('.720p') != -1:
     short_name = full_name[:full_name.find('.720p')]
+    found = True
+
+if full_name.find('.HDTV') != -1 and not found:
+    short_name = full_name[:full_name.find('.HDTV')]
+
 
 print short_name
